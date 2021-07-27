0e9b60287f439dff02360aa896894f63d24e9e6a
Soumajit Ghosh <soumojitghosh02@gmail.com>
2021-07-27 15:18:43 +0530

    font safe and comments added

--- brain.py
+++ brain.py
@@ -1,21 +1,22 @@
 import os
 import threading
 import time
-import sys
-from custom_widget import Ui_pogress_widget
 import cv2
 from PIL import Image
-from PyQt5 import QtWidgets
 
 
+# This class converts images main process happens here used in ui.py --> convert_fnc()
 class Brain:
-    def __init__(self, frame):
-        self.warning = frame
+    def __init__(self, warning_popup):
+        # Warning function from ui.py
+        self.warning = warning_popup
 
     def run(self, img_instance, progress_layout):
+        """Converts Images and saves on saving path of img_instance"""
 
-        # Convert to OpenCV Image
+        # Open as OpenCV Image
         image = cv2.imread(img_instance.image)
+        # Popup progressbar changed
         progress_layout.on_count_changed(6)
 
         # Loading Model
@@ -23,33 +24,47 @@ class Brain:
         model_path = "assets/models/FSRCNN_x4.pb"
         sr.readModel(model_path)
         sr.setModel("fsrcnn", 4)
+        # Popup progressbar changed
         progress_layout.on_count_changed(21)
 
-        def thread(image):
-
-            # Upscaling Image
-            result_img = sr.upsample(image)
+        def thread(image_input):
+            """Worker Thread Function for Upscalling Image"""
+            # Scaling Image
+            result_img = sr.upsample(image_input)
+            # Saving Upscale Model Image
             cv2.imwrite("output/image.png", result_img)
 
+        # Running Thread Function on diffrent Thread
         th_1 = threading.Thread(target=thread, args=[image])
         th_1.start()
         th_1.join()
+        # Popup progressbar changed
         progress_layout.on_count_changed(61)
+        # Waiting for 1 sec just in case
+        time.sleep(1)
 
         # Resizing Image
         img = Image.open("output/image.png")
+        # Popup progressbar changed
         progress_layout.on_count_changed(71)
+
+        # Trying to Catch If Text is inserted in resolution field
         try:
             img = img.resize((int(img_instance.img_width), int(img_instance.img_height)))
         except ValueError:
             self.warning("Invalid Resolution!")
         else:
+            # Popup progressbar changed
             progress_layout.on_count_changed(80)
+
+            # Trying to catch if invalid path has been inputted
             try:
                 img.save(img_instance.saving_path)
                 progress_layout.on_count_changed(89)
             except FileNotFoundError:
                 self.warning("Invalid Saving Path!")
         finally:
+            # Popup progressbar changed
             progress_layout.on_count_changed(99)
+            # Deleting the temporary image
             os.remove("output/image.png")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 import ui
 import sys
 
@@ -6,6 +6,13 @@ import sys
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
+
+    QtGui.QFontDatabase.addApplicationFont('assets/Poppins/Poppins-Bold.ttf')
+    QtGui.QFontDatabase.addApplicationFont('assets/Poppins/Poppins-Black.ttf')
+    QtGui.QFontDatabase.addApplicationFont('assets/Poppins/Poppins-SemiBold.ttf')
+    QtGui.QFontDatabase.addApplicationFont('assets/Poppins/Poppins-Regular.ttf')
+    QtGui.QFontDatabase.addApplicationFont('assets/Poppins/Poppins-Medium.ttf')
+
     ui = ui.Ui_MainWindow(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
